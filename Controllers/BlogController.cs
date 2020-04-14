d7196a9669e6569fd5221192e38e833a84c3398d
tmorrisiii86 <61118010+tmorrisiii86@users.noreply.github.com>
2020-04-14 14:10:50 -0500

    Add files via upload

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DailyBlogger.Models;
+
+namespace DailyBlogger.Controllers
+{
+    public class BlogController : Controller
+    {
+        private BlogContext _context;
+
+        public BlogController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult List()
+        {
+            IEnumerable<BlogPost> posts = _context.blogPost.ToList<BlogPost>();
+            return View(posts);
+        }
+        
+        public IActionResult New()
+        {
+            BlogPost blogPost = new BlogPost();
+            return View(blogPost);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("blog_title,content,blog_date")] BlogPost blog)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(blog);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(List));
+            }
+            return View(blog);
+        }
+
+        public IActionResult Details(int blog_id)
+        {
+            BlogPost blogPost = _context.blogPost.Find(blog_id);
+            return View(blogPost);
+        }
+
+        public IActionResult Edit(int blog_id)
+        {
+            BlogPost blogPost = _context.blogPost.Find(blog_id);
+            return View(blogPost);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update([Bind("blog_title,content,blog_date,blog_id")] BlogPost blog)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Update(blog);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(List));
+            }
+            return View(blog);
+        }
+
+        public IActionResult Delete([Bind("blog_id")] int blog_id)
+        {
+            BlogPost blogPost = _context.blogPost.Find(blog_id);
+            _context.Remove(blogPost);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(List));
+        }
+    }
+}
---
+++ Controllers/ExampleController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyBlogger.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public IActionResult ShowMeAPage()
+        {
+            return View();
+        }
+
+        public IActionResult ShowMeAnError()
+        {
+            return NotFound();
+        }
+
+        public IActionResult GiveMeAFile()
+        {
+            return File("/mydoc.txt", "text/plain");
+        }
+
+        public IActionResult GiveMeSomeJson()
+        {
+            string jsondata = "(Name: John-Luc Picard, Rank: Captain, Starship: Enterprise)";
+            return Json(jsondata);
+        }
+
+        public IActionResult TakeMeToGoogle()
+        {
+            return Redirect("https://google.com");
+        }
+    }
+}
