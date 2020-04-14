96967321b33422a4dd4e4b871098095435bbd82f
tmorrisiii86 <61118010+tmorrisiii86@users.noreply.github.com>
2020-04-14 14:04:29 -0500

    Add files via upload

---
+++ Models/BlogPost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DailyBlogger.Models
+{
+    public class BlogPost
+    {
+        [Key]
+        public int blog_id { get; set; }
+        public string blog_title { get; set; }
+        public string content { get; set; }
+        public DateTime blog_date { get; set; }
+    }
+}
